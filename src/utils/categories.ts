d02df970d2683d8804684f7969ac1409c1a254bf
Matheus Altair <64233836+matheusaltair@users.noreply.github.com>
2023-01-25 21:23:48 -0300

    categories file

---
+++ src/utils/categories.ts
@@ -0,0 +1,8 @@
+export const categories = [
+  { key: 'purchase', name: 'Compras', icon: 'shopping-bag', color: '#5636D3' },
+  { key: 'food', name: 'Alimentação', icon: 'coffee', color: '@FF872C' },
+  { key: 'salary', name: 'Sálario', icon: 'dollar-sign', color: '#12A454' },
+  { key: 'car', name: 'Carro', icon: 'crosshair', color: '#E83F58' },
+  { key: 'leisure', name: 'Lazer', icon: 'heart', color: '#26195C' },
+  { key: 'studies', name: 'Estudos', icon: 'book', color: '#9C001A' }
+]
