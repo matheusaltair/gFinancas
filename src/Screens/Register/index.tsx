2753b817d8405222a03b3907496a48b9eab01dd0
Matheus Altair <64233836+matheusaltair@users.noreply.github.com>
2023-02-02 11:41:34 -0300

    async-storage

--- src/Screens/Register/index.tsx
+++ src/Screens/Register/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Modal } from 'react-native';
 import * as Yup from 'yup'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { yupResolver } from '@hookform/resolvers/yup'
 import { Button } from '../../components/Form/Button';
-import { Input } from '../../components/Form/Input';
 import { InputForm } from '../../components/Form/InputForm';
 import { InputSelect } from '../../components/Form/InputSelect';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
@@ -23,6 +24,7 @@ const schema = Yup.object().shape({
 })
 
 export function Register() {
+  const dataKey = '@gFinancas:transaction'
 
   const { control, handleSubmit, formState: { errors } } = useForm({
     resolver: yupResolver(schema)
@@ -36,16 +38,40 @@ export function Register() {
   const [categoryModalOpen, setCategoryModalOpen] = useState(false)
 
 
-  function handleRegister(form: FormData) {
-    const data = {
-      transactionType,
+  async function handleRegister(form: FormData) {
+    const newData = {
       name: form.name,
       amount: form.amount,
+      transactionType,
       category: category.key
     }
-    console.log(data)
+    console.log(newData)
+
+    try {
+      const data = await AsyncStorage.getItem(dataKey)
+      const currentData = data ? JSON.parse(data) : []
+
+      const AllData = [
+        ...currentData,
+        newData
+      ]
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(AllData))
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 
+  useEffect(() => {
+    async function getAsync() {
+
+      const Items = await AsyncStorage.getItem(dataKey)
+      console.log(JSON.parse(Items!))
+    }
+    getAsync()
+
+  }, []);
   function handleTransactionType(type: 'up' | 'down') {
     setTransactionType(type)
   }
@@ -75,7 +101,6 @@ export function Register() {
             control={control}
             placeholder='Preço'
             error={errors.amount && errors.amount.message}
-
           />
           <TransactionType>
             <TransactionTypeButton
